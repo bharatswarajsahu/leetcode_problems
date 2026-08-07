2c071cef0b9e78802b44629de0320db3949eb244
Bharat swaraj Sahu <bharatswarajsahu@gmail.com>
2026-08-07 10:00:17 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/2706. Buy Two Chocolates/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+
+        int minCost = INT_MAX;
+
+        for(int i = 0; i < prices.size(); i++) {
+
+            for(int j = i + 1; j < prices.size(); j++) {
+
+                int cost = prices[i] + prices[j];
+
+                if(cost <= money) {
+                    minCost = min(minCost, cost);
+                }
+            }
+        }
+
+        if(minCost == INT_MAX)
+            return money;
+
+        return money - minCost;
+    }
+};
