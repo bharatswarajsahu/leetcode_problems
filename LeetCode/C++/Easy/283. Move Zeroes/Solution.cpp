3ada9a7d52dd33fc014827bae130585bd73e6446
Bharat swaraj Sahu <bharatswarajsahu@gmail.com>
2026-09-06 20:07:07 +0530

    Add Easy C++ Solution for 283. Move Zeroes from LeetCode

--- LeetCode/C++/Easy/283. Move Zeroes/Solution.cpp
+++ LeetCode/C++/Easy/283. Move Zeroes/Solution.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int j = 0;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
             }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
         }
 
-        while (j < nums.size()) {
-            nums[j] = 0;
-            j++;
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
